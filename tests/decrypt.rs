b4c682b1f099ad61155e8e6a23454fcee09f3895
Shun Sakai <sorairolake@protonmail.ch>
2022-10-14 13:21:42 +0900

    Update test

--- tests/decrypt.rs
+++ tests/decrypt.rs
@@ -20,20 +20,32 @@ const TEST_DATA: &[u8] = include_bytes!("data/data.txt");
 // Generated using `scrypt` version 1.3.1.
 const TEST_DATA_ENC: &[u8] = include_bytes!("data/data.txt.enc");
 
-fn decrypt(password: &[u8], data: &[u8]) -> Result<Vec<u8>, Error> {
-    let cipher = Decryptor::new(password, data)?;
-    cipher.decrypt_to_vec()
-}
-
 #[test]
 fn success() {
-    let decrypted = decrypt(PASSWORD, TEST_DATA_ENC).unwrap();
+    let cipher = Decryptor::new(PASSWORD, TEST_DATA_ENC).unwrap();
+    let mut buf = vec![u8::default(); cipher.out_len()];
+    cipher.decrypt(&mut buf).unwrap();
+    assert_eq!(buf, TEST_DATA);
+
+    let decrypted = Decryptor::new(PASSWORD, TEST_DATA_ENC)
+        .and_then(Decryptor::decrypt_to_vec)
+        .unwrap();
     assert_eq!(decrypted, TEST_DATA);
 }
 
 #[test]
+#[should_panic]
+fn invalid_output_length() {
+    let cipher = Decryptor::new(PASSWORD, TEST_DATA_ENC).unwrap();
+    let mut buf = vec![u8::default(); cipher.out_len() + 1];
+    cipher.decrypt(&mut buf).unwrap();
+}
+
+#[test]
 fn incorrect_password() {
-    let decrypted = decrypt(b"passphrase", TEST_DATA_ENC).unwrap_err();
+    let decrypted = Decryptor::new("passphrase", TEST_DATA_ENC)
+        .and_then(Decryptor::decrypt_to_vec)
+        .unwrap_err();
     assert_eq!(
         decrypted.to_string(),
         Error::InvalidSignature(MacError).to_string()
@@ -43,7 +55,9 @@ fn incorrect_password() {
 #[test]
 fn invalid_length() {
     let data: [u8; 127] = [u8::default(); 127];
-    let decrypted = decrypt(PASSWORD, &data).unwrap_err();
+    let decrypted = Decryptor::new(PASSWORD, data)
+        .and_then(Decryptor::decrypt_to_vec)
+        .unwrap_err();
     assert_eq!(decrypted.to_string(), Error::InvalidLength(127).to_string());
 }
 
@@ -51,7 +65,9 @@ fn invalid_length() {
 fn invalid_magic_number() {
     let mut data = TEST_DATA_ENC.to_vec();
     data[0] = u32::from('b').try_into().unwrap();
-    let decrypted = decrypt(PASSWORD, &data).unwrap_err();
+    let decrypted = Decryptor::new(PASSWORD, data)
+        .and_then(Decryptor::decrypt_to_vec)
+        .unwrap_err();
     assert_eq!(decrypted.to_string(), Error::InvalidMagicNumber.to_string());
 }
 
@@ -59,7 +75,9 @@ fn invalid_magic_number() {
 fn unknown_version() {
     let mut data = TEST_DATA_ENC.to_vec();
     data[6] = 1;
-    let decrypted = decrypt(PASSWORD, &data).unwrap_err();
+    let decrypted = Decryptor::new(PASSWORD, data)
+        .and_then(Decryptor::decrypt_to_vec)
+        .unwrap_err();
     assert_eq!(decrypted.to_string(), Error::UnknownVersion(1).to_string());
 }
 
@@ -69,7 +87,9 @@ fn invalid_params() {
 
     let mut data_1 = data.clone();
     data_1[7] = 65;
-    let decrypted = decrypt(PASSWORD, &data_1).unwrap_err();
+    let decrypted = Decryptor::new(PASSWORD, data_1)
+        .and_then(Decryptor::decrypt_to_vec)
+        .unwrap_err();
     assert_eq!(
         decrypted.to_string(),
         Error::InvalidParams(InvalidParams).to_string()
@@ -77,7 +97,9 @@ fn invalid_params() {
 
     let mut data_2 = data.clone();
     data_2[8..12].copy_from_slice(&u32::to_be_bytes(0));
-    let decrypted = decrypt(PASSWORD, &data_2).unwrap_err();
+    let decrypted = Decryptor::new(PASSWORD, data_2)
+        .and_then(Decryptor::decrypt_to_vec)
+        .unwrap_err();
     assert_eq!(
         decrypted.to_string(),
         Error::InvalidParams(InvalidParams).to_string()
@@ -85,7 +107,9 @@ fn invalid_params() {
 
     let mut data_3 = data;
     data_3[12..16].copy_from_slice(&u32::to_be_bytes(0));
-    let decrypted = decrypt(PASSWORD, &data_3).unwrap_err();
+    let decrypted = Decryptor::new(PASSWORD, data_3)
+        .and_then(Decryptor::decrypt_to_vec)
+        .unwrap_err();
     assert_eq!(
         decrypted.to_string(),
         Error::InvalidParams(InvalidParams).to_string()
@@ -98,7 +122,9 @@ fn invalid_checksum() {
     let mut checksum: [u8; 16] = data[48..64].try_into().unwrap();
     checksum.reverse();
     data[48..64].copy_from_slice(&checksum);
-    let decrypted = decrypt(PASSWORD, &data).unwrap_err();
+    let decrypted = Decryptor::new(PASSWORD, data)
+        .and_then(Decryptor::decrypt_to_vec)
+        .unwrap_err();
     assert_eq!(decrypted.to_string(), Error::InvalidChecksum.to_string());
 }
 
@@ -108,7 +134,9 @@ fn invalid_signature() {
     let mut signature: [u8; 32] = data[64..96].try_into().unwrap();
     signature.reverse();
     data[64..96].copy_from_slice(&signature);
-    let decrypted = decrypt(PASSWORD, &data).unwrap_err();
+    let decrypted = Decryptor::new(PASSWORD, data)
+        .and_then(Decryptor::decrypt_to_vec)
+        .unwrap_err();
     assert_eq!(
         decrypted.to_string(),
         Error::InvalidSignature(MacError).to_string()
--- tests/encrypt.rs
+++ tests/encrypt.rs
@@ -15,53 +15,74 @@ use scrypt::Params;
 use scryptenc::Encryptor;
 use sha2::{Digest, Sha256};
 
-fn encrypt() -> Encryptor {
-    let password = "password";
-    let data = "Hello, world!";
+const PASSWORD: &str = "password";
+const DATA: &str = "Hello, world!";
 
-    let params = Params::new(10, 1, 1).unwrap();
-    Encryptor::with_params(password, params, data)
+#[test]
+fn success() {
+    let cipher = Encryptor::with_params(PASSWORD, Params::new(10, 1, 1).unwrap(), DATA);
+    let mut buf = vec![u8::default(); cipher.out_len()];
+    cipher.encrypt(&mut buf);
+    assert_eq!(buf.len(), DATA.len() + 128);
+
+    let encrypted =
+        Encryptor::with_params(PASSWORD, Params::new(10, 1, 1).unwrap(), DATA).encrypt_to_vec();
+    assert_eq!(encrypted.len(), DATA.len() + 128);
+}
+
+#[test]
+#[should_panic]
+fn invalid_output_length() {
+    let cipher = Encryptor::with_params(PASSWORD, Params::new(10, 1, 1).unwrap(), DATA);
+    let mut buf = vec![u8::default(); cipher.out_len() - 1];
+    cipher.encrypt(&mut buf);
 }
 
 #[test]
 fn magic_number() {
-    let encrypted = encrypt().encrypt_to_vec();
+    let encrypted =
+        Encryptor::with_params(PASSWORD, Params::new(10, 1, 1).unwrap(), DATA).encrypt_to_vec();
     assert_eq!(&encrypted[..6], b"scrypt");
 }
 
 #[test]
 fn version() {
-    let encrypted = encrypt().encrypt_to_vec();
+    let encrypted =
+        Encryptor::with_params(PASSWORD, Params::new(10, 1, 1).unwrap(), DATA).encrypt_to_vec();
     assert_eq!(encrypted[6], 0);
 }
 
 #[test]
 fn log_n() {
-    let encrypted = encrypt().encrypt_to_vec();
+    let encrypted =
+        Encryptor::with_params(PASSWORD, Params::new(10, 1, 1).unwrap(), DATA).encrypt_to_vec();
     assert_eq!(encrypted[7], 10);
 }
 
 #[test]
 fn r() {
-    let encrypted = encrypt().encrypt_to_vec();
+    let encrypted =
+        Encryptor::with_params(PASSWORD, Params::new(10, 1, 1).unwrap(), DATA).encrypt_to_vec();
     assert_eq!(&encrypted[8..12], u32::to_be_bytes(1));
 }
 
 #[test]
 fn p() {
-    let encrypted = encrypt().encrypt_to_vec();
+    let encrypted =
+        Encryptor::with_params(PASSWORD, Params::new(10, 1, 1).unwrap(), DATA).encrypt_to_vec();
     assert_eq!(&encrypted[12..16], u32::to_be_bytes(1));
 }
 
 #[test]
 fn checksum() {
-    let encrypted = encrypt().encrypt_to_vec();
+    let encrypted =
+        Encryptor::with_params(PASSWORD, Params::new(10, 1, 1).unwrap(), DATA).encrypt_to_vec();
     let checksum = Sha256::digest(&encrypted[..48]);
     assert_eq!(&encrypted[48..64], &checksum[..16]);
 }
 
 #[test]
 fn out_len() {
-    let cipher = encrypt();
+    let cipher = Encryptor::with_params(PASSWORD, Params::new(10, 1, 1).unwrap(), DATA);
     assert_eq!(cipher.out_len(), 141);
 }
