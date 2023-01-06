db92e13bd26496660b48da0ca069cbe740c43d45
Shun Sakai <sorairolake@protonmail.ch>
2023-01-06 23:32:09 +0900

    Update copyright notice

--- examples/decrypt.rs
+++ examples/decrypt.rs
@@ -1,7 +1,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 //
-// Copyright (C) 2022 Shun Sakai
+// Copyright (C) 2022-2023 Shun Sakai
 //
 
 //! An example of decrypting a file from the scrypt encrypted data format.
