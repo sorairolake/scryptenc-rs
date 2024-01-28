c78d427c8f6b57bdf70636a4e48e5e411334f33e
Shun Sakai <sorairolake@protonmail.ch>
2024-01-28 09:15:35 +0900

    Update dependencies for Wasm examples

--- crates/wasm/examples/decrypt.ts
+++ crates/wasm/examples/decrypt.ts
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-import * as cli from "https://deno.land/std@0.212.0/cli/mod.ts";
+import * as cli from "https://deno.land/std@0.213.0/cli/mod.ts";
 
 import * as scryptenc from "../pkg/scryptenc_wasm.js";
 
