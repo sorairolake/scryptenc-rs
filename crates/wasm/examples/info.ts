e47c1c755f6349eb84b047332f68d6809f6e5f2d
Shun Sakai <sorairolake@protonmail.ch>
2024-01-30 16:09:07 +0900

    Update Wasm examples

--- crates/wasm/examples/info.ts
+++ crates/wasm/examples/info.ts
@@ -2,9 +2,20 @@
 //
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-import * as scryptenc from "../pkg/scryptenc_wasm.js";
+import { command, io, scryptenc } from "./deps.ts";
 
-const ciphertext = Deno.readFileSync(Deno.args[0]);
+import { VERSION } from "./version.ts";
+
+const { args } = await new command.Command()
+  .name("info")
+  .version(VERSION)
+  .description("An example of reading the scrypt parameters.")
+  .arguments("[FILE:file]")
+  .parse();
+
+const ciphertext = args[0] === undefined
+  ? io.readAllSync(Deno.stdin)
+  : Deno.readFileSync(args[0]);
 
 const params = new scryptenc.Params(ciphertext);
 console.log(
